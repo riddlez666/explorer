70626358001d2ab263bd4e8a31c9c16f2b0f26c0
Derek May <32908855+riddlez666@users.noreply.github.com>
2018-02-19 05:58:43 -0800

    Change to minty theme

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ var config = function () {
   this.ipcPath = process.env["HOME"] + "/.local/share/io.parity.ethereum/jsonrpc.ipc";
   this.provider = new web3.providers.IpcProvider(this.ipcPath, net);
   
-  this.bootstrapUrl = "https://maxcdn.bootstrapcdn.com/bootswatch/3.3.7/yeti/bootstrap.min.css";
+  this.bootstrapUrl = "https://bootswatch.com/4/minty/bootstrap.min.css";
   
   this.names = {
   }
